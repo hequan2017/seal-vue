c282074cef2327a7ad0ac209e489cea5d8bc08dc
何全 <hequan@aipark.com>
2019-06-06 16:49:21 +0800

    .

--- src/config/index.js
+++ src/config/index.js
@@ -6,7 +6,7 @@ export default {
   /**
    * @description token在Cookie中存储的天数，默认1天
    */
-  cookieExpires: 3,
+  cookieExpires: 1,
   /**
    * @description 是否使用国际化，默认为false
    *              如果不使用，则需要在路由中给需要在菜单中展示的路由设置meta: {title: 'xxx'}
@@ -18,7 +18,7 @@ export default {
    */
   baseUrl: {
     // dev: 'https://www.easy-mock.com/mock/5add9213ce4d0e69998a6f51/iview-admin/',
-    dev: 'http://129.28.156.219:8001',
+    dev: 'http://192.168.100.90:8000',
     pro: 'http://129.28.156.219:8001'
   },
   /**
